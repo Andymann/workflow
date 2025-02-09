5000682efffac5754c788b58e81efe311111fd99
Andymann <andreas.fischer@fielmann.com>
2025-02-09 19:43:54 +0100

    play with script

--- build_of_rpi64.sh
+++ build_of_rpi64.sh
@@ -3,7 +3,6 @@
 sudo apt-get clean -y
 sudo apt-get update
 sudo apt-get upgrade -y
-
 export URL=https://github.com/openframeworks/openFrameworks/releases/download/0.12.0/of_v0.12.0_linuxaarch64_release.tar.gz
 curl -L -o ofx.tar.gz $URL
 tar -xf ofx.tar.gz
@@ -17,7 +16,7 @@ pkill yes
 # add jack for compiling midistuff
 cd $OF_ROOT/libs/openFrameworksCompiled/project/makefileCommon/
 sed -i 's/PLATFORM_PKG_CONFIG_LIBRARIES += libcurl/PLATFORM_PKG_CONFIG_LIBRARIES += libcurl\nPLATFORM_PKG_CONFIG_LIBRARIES += jack/' config.linux.common.mk
-# make Release -C $OF_ROOT/libs/openFrameworksCompiled/project
+make Release -C $OF_ROOT/libs/openFrameworksCompiled/project
 cd $OF_ROOT/examples/3d/3DPrimitivesExample/
 #for ssh:
 export DISPLAY=:0
